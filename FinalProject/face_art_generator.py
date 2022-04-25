f02d24f01935fdf8c5b4f658e0efe33aad3e5f5c
Sudar Kartheepan <sudarkartheepan@gmail.com>
2022-04-25 13:40:39 -0700

    Added Discriminator and Generator for DCGAN

--- FinalProject/face_art_generator.py
+++ FinalProject/face_art_generator.py
@@ -2,5 +2,44 @@ import tensorflow as tf
 import numpy as np
 import PIL
 import imageio
-from tensorflow.keras import layers
+from tensorflow.python import keras
+from keras import layers
 
+#https://www.tensorflow.org/tutorials/generative/dcgan
+#Creating sample discriminator for GAN
+
+discriminator = tf.keras.Sequential()
+discriminator.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same',
+                                     input_shape=[28, 28, 1]))
+discriminator.add(layers.LeakyReLU())
+discriminator.add(layers.Dropout(0.3))
+
+discriminator.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
+discriminator.add(layers.LeakyReLU())
+discriminator.add(layers.Dropout(0.3))
+
+discriminator.add(layers.Flatten())
+discriminator.add(layers.Dense(1))
+
+#Creating sample generator for GAN
+
+generator = tf.keras.Sequential()
+generator.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+generator.add(layers.BatchNormalization())
+generator.add(layers.LeakyReLU())
+
+generator.add(layers.Reshape((7, 7, 256)))
+assert generator.output_shape == (None, 7, 7, 256)
+
+generator.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+assert generator.output_shape == (None, 7, 7, 128)
+generator.add(layers.BatchNormalization())
+generator.add(layers.LeakyReLU())
+
+generator.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+assert generator.output_shape == (None, 14, 14, 64)
+generator.add(layers.BatchNormalization())
+generator.add(layers.LeakyReLU())
+
+generator.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+assert generator.output_shape == (None, 28, 28, 1)
