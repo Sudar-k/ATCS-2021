734032c12d3a8b63fce4b28aacc98b520eb40e0b
Sudar Kartheepan <sudarkartheepan@gmail.com>
2022-04-29 09:58:03 -0700

    Added webcam feature for facial recognition

--- FinalProject/face_art_generator.py
+++ FinalProject/face_art_generator.py
@@ -50,31 +50,42 @@ assert generator.output_shape == (None, 28, 28, 1)
 
 #Detect Faces
 
-# Get user supplied values
-imagePath = "multiple.png"
+# imagePath = "multiple.png"
+
 cascPath = "haarcascade_frontalface_default.xml"
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 # Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# image = cv2.imread(imagePath)'
 
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
+#Use Webcam
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
     gray,
     scaleFactor=1.1,
     minNeighbors=4,
     minSize=(30, 30),
 
-)
+    )
 
-print("Found {0} faces!".format(len(faces)))
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.imshow("Video", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
